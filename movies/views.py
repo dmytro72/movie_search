a13af083a5fab1d4f018510d67e61e87a0a0f322
Dmytro Shaban <dmytro.shaban@gmail.com>
2025-06-16 20:33:02 +0200

    Implemented pagination for search

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from django.http import JsonResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Film, Actor
 from .utils import normalize
 
@@ -13,25 +13,45 @@ def search_view(request):
     query = request.GET.get('q', '').strip()
     films = []
     actors = []
+    films_page = None
+    actors_page = None
 
     if query:
         normalized_query = normalize(query)
         films = Film.objects.filter(
             title_normalized__icontains=normalized_query
-            ).order_by('title')
-        
+        ).order_by('title')
         actors = Actor.objects.filter(
             name_normalized__icontains=normalized_query
-            ).order_by('name')
-        
+        ).order_by('name')
+
+        film_paginator = Paginator(films, 25)
+        actor_paginator = Paginator(actors, 25)
+
+        film_page_number = request.GET.get('film_page')
+        actor_page_number = request.GET.get('actor_page')
+
+        try:
+            films_page = film_paginator.get_page(film_page_number)
+        except PageNotAnInteger:
+            films_page = film_paginator.page(1)
+        except EmptyPage:
+            films_page = film_paginator.page(film_paginator.num_pages)
+
+        try:
+            actors_page = actor_paginator.get_page(actor_page_number)
+        except PageNotAnInteger:
+            actors_page = actor_paginator.page(1)
+        except EmptyPage:
+            actors_page = actor_paginator.page(actor_paginator.num_pages)
+
     context = {
         'query': query,
-        'films': films,
-        'actors': actors,
+        'films': films_page,
+        'actors': actors_page,
         'films_count': films.count() if films else 0,
         'actors_count': actors.count() if actors else 0,
     }
-
     return render(request, 'movies/search.html', context)
 
 
